07088815665f2597787b8f158ccddcaaa53025db
Cristian <cristianotona@gmail.com>
2022-10-26 19:03:38 +0200

    components

---
+++ src/app/App.tsx
@@ -0,0 +1,12 @@
+import { Adviser } from "../components/adviser";
+
+
+export function App() {
+    return (
+        <>
+            <Adviser></Adviser>
+        </>
+    );
+}
+
+export default App;
